c18cb59774d720225a4f50e2e37cd60d458d10df
statkwon <statkwon96@gmail.com>
2025-11-21 10:26:05 -0600

    [Update] modified parameters & documentation for loco()

--- R/loco.R
+++ R/loco.R
@@ -2,12 +2,13 @@
 #'
 #' @param X feature matrix \eqn{X} of dimension \eqn{n\times p}
 #' @param y target vector \eqn{Y} of length \eqn{n}
-#' @param mean.fun mean function \eqn{\hat{\mu}} fitted with \eqn{X} and \eqn{y}
-#' @param refitted.fun mean function \eqn{\hat{\mu}_{(-j)}} to refit with \eqn{X(-j)} and \eqn{y}
+#' @param mean.fun mean function \eqn{\hat{\mu}}
+#' @param n.train \eqn{n_\text{train}} rows of \eqn{X} are randomly chosen and used to fit \eqn{\hat{\mu}},
+#' another \eqn{n_\text{cal}=n-n_\text{train}} rows are used as a calibration set
 #'
-#' @return prediction interval matrix \eqn{W} of size \eqn{n\times p}
+#' @return array of \eqn{p} matrices with conformal prediction intervals, \eqn{W_1, \ldots, W_p}, of size \eqn{n_\text{train}\times2}
 #' @export
-loco <- function(X, y, mean.fun, refitted.fun) {
-  W = NULL
+loco <- function(X, y, mean.fun, n.train) {
+  W = array(dim = c(p, n.train, 2))
   return(W)
 }
